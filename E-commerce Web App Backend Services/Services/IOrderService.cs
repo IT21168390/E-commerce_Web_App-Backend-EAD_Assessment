83575d6c2a44921441c84918970c81b80a4b3f44
IT21168390 <it21168390@my.sliit.lk>
2024-09-27 23:25:08 +0530

    Customer Orders: Create new Order

---
+++ E-commerce Web App Backend Services/Services/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using E_commerce_Web_App_Backend_Services.Dto;
+using E_commerce_Web_App_Backend_Services.models;
+
+namespace E_commerce_Web_App_Backend_Services.Services
+{
+    public interface IOrderService
+    {
+        Task<Order> PlaceOrderAsync(PlaceOrderDto placeOrderDto);
+    }
+}
