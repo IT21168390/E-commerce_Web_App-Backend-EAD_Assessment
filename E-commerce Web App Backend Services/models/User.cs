b1b877e0f648642e15d127b3aee10fac827ca198
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-09-29 22:25:44 +0530

    Made necessary changes

--- E-commerce Web App Backend Services/models/User.cs
+++ E-commerce Web App Backend Services/models/User.cs
@@ -26,58 +26,6 @@ namespace E_commerce_Web_App_Backend_Services
         [BsonElement("account_created_at")]
         public DateTime? AccountCreatedAt { get; set; }
 
-        // Embedded document for Vendor details
-        [BsonElement("vendor_details")]
-        public VendorDetails? VendorDetails { get; set; }
-
-        // Embedded document for Customer details
-        [BsonElement("customer_details")]
-        public CustomerDetails? CustomerDetails { get; set; }
-    }
-
-    public class VendorDetails
-    {
-        [BsonElement("company_name")]
-        public string? CompanyName { get; set; }
-
-        [BsonElement("vendor_rating")]
-        public double? VendorRating { get; set; }
-
-        [BsonElement("products")]
-        public List<VendorProduct>? Products { get; set; }
-    }
-
-    public class VendorProduct
-    {
-        [BsonElement("product_id")]
-        public string? ProductId { get; set; }
-
-        [BsonElement("name")]
-        public string? Name { get; set; }
-
-        [BsonElement("rating")]
-        public double Rating { get; set; }
-    }
-
-    public class CustomerDetails
-    {
-        [BsonElement("shipping_address")]
-        public Address ShippingAddress { get; set; }
-
-        [BsonElement("order_history")]
-        public List<OrderHistory> OrderHistory { get; set; }
-    }
-
-    public class OrderHistory
-    {
-        [BsonElement("order_id")]
-        public string OrderId { get; set; }
-
-        [BsonElement("status")]
-        public string Status { get; set; } // "delivered", "processing", etc.
-
-        [BsonElement("total_amount")]
-        public double TotalAmount { get; set; }
     }
 
     public class Address
