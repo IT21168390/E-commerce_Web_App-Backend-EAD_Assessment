00576ccb5bb87c7b95d2847482b42bec3aaa09b3
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-10-03 07:51:45 +0530

    Added notification service to other services

---
+++ E-commerce Web App Backend Services/constants/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace E_commerce_Web_App_Backend_Services.constants
+{
+    //***notification***//
+    public class Constant
+    {
+        //{todo: please update with a valid vendor id}
+        public const string VendorId = "";
+    }
+}
