aa4b362086cab047a97e3ef6556057d696bd1948
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-10-03 07:54:11 +0530

    Added Dto classes do to inventory updates

---
+++ E-commerce Web App Backend Services/Dto/InventoryDto.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace E_commerce_Web_App_Backend_Services.Dto
+{
+    public class InventoryDto
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string ProductId { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
